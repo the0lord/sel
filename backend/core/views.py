e4c28b60db0374b46d3072ef6e326660fbf220e2
Firuz Azizbekov <firuz.azizbekov_2026@ucentralasia.org>
2024-11-08 21:54:07 +0600

    base

---
+++ backend/core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def api_home(request):
+    return Response({"message": "Hello, welcome to the API!"})
