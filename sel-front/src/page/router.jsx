6df1b5682898ac7bd11d4c0c94d83fda2a73ee09
edyshekeev <edyshekeev@gmail.com>
2024-11-08 23:01:02 +0600

    Needs page and list additional

--- sel-front/src/page/router.jsx
+++ sel-front/src/page/router.jsx
@@ -2,14 +2,16 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import BaseLayout from "widgets/layouts/BaseLayout";
 import { HomePage } from "./home";
 import { ProductsPage } from "./products";
+import { NeedsPage } from "./needs";
 
 const Router = () => {
     return <BaseLayout>
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/products" element={<ProductsPage />} />
+            <Route path="/needs" element={<NeedsPage />} />
             <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
     </BaseLayout>
 }
-export default Router;          
+export default Router;
