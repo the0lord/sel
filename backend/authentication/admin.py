6abe5c4c8c786c6277e05afb041b085246789f48
Firuz Azizbekov <firuz.azizbekov_2026@ucentralasia.org>
2024-11-08 22:15:36 +0600

    .

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'role', 'is_staff']
+    list_filter = ['role', 'is_staff', 'is_superuser']
+    search_fields = ['username', 'email']
+    ordering = ['username']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
