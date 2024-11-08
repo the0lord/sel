49ca2d735f0677e0b4f82341ab9b7689fee8db70
Firuz Azizbekov <firuz.azizbekov_2026@ucentralasia.org>
2024-11-09 04:36:27 +0600

    a

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductViewSet, NeedListViewSet, FarmerStackViewSet
+from .views import ProductViewSet, NeedListViewSet, FarmerStackViewSet, RegionViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
 router.register(r'needlists', NeedListViewSet)
 router.register(r'farmerstacks', FarmerStackViewSet)
+router.register(r'region', RegionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
