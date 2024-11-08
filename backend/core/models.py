6f76f98fe19502bd64ad5848e268e1b00263e920
the0lord <alisher0lord@gmail.com>
2024-11-08 22:58:59 +0600

    user

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+User = get_user_model()
 class Product(models.Model):
     name = models.CharField(max_length=100)
     measurement_unit = models.CharField(max_length=50)
--- backend/core/views.py
+++ backend/core/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import Product, NeedList, FarmerStack
 from .serializers import ProductSerializer, NeedListSerializer, FarmerStackSerializer
-from .permissions import IsAdminOrReadOnly, IsStaffOrReadOnly
+from .permissions import IsAdminOrReadOnly, IsStaffOrReadOnly, FarmerStackPermission
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
