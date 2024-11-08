9a79d637a1f0656fcac60d30c1aaddb612770ef2
edyshekeev <edyshekeev@gmail.com>
2024-11-09 03:35:52 +0600

    Farmer stack update

--- sel-front/src/shared/store/store.js
+++ sel-front/src/shared/store/store.js
@@ -2,12 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './reducer/auth.reducer';
 import usersSlice from './reducer/users.reduder';
 import productsSlice from './reducer/products.reduder';
+import needsSlice from './reducer/needs.reducer';
+import stackSlice from './reducer/stack.reducer';
 
 export const store = configureStore({
     reducer: {
         auth: authSlice.reducer,
         users: usersSlice.reducer,
         products: productsSlice.reducer,
+        needs: needsSlice.reducer,
+        stack: stackSlice.reducer,
         // users: 
         // sanctions: sanctionsSlice.reducer,
         // whiteList: whiteListSlice.reducer,
