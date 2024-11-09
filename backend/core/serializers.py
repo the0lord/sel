e9a64073d1f6fc7710238c1efc15708da1f934ee
Firuz Azizbekov <firuz.azizbekov_2026@ucentralasia.org>
2024-11-09 06:59:17 +0600

    a

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -44,8 +44,25 @@ class NeedListSerializer(serializers.ModelSerializer):
 
 class FarmerStackSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField(required=False)
-    product = ProductSerializer(read_only=True)
-    region = RegionSerializer(read_only=True)
+    product = ProductSerializer(required=False)
+    region = RegionSerializer(required=False)
+    product_id = serializers.IntegerField(required=False)
+    region_id = serializers.IntegerField(required=False)
+
+    def create(self, validated_data):
+        if 'product_id' in validated_data:
+            product = Product.objects.get(pk = validated_data['product_id'])
+        else:
+            product = validated_data['product']
+        if 'region_id' in validated_data:
+            region = Region.objects.get(pk = validated_data['region_id'])
+        else:
+            region = validated_data['region']
+        return FarmerStack.objects.create(
+            product = product,
+            region = region,
+            **validated_data
+        )
 
     class Meta:
         model = FarmerStack
